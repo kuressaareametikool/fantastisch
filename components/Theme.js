fbb1b5e80b0f92fea780bef712c63eea9bc1df13
Angela <a.joeorg@hotmail.com>
2019-01-25 10:27:41 +0200

    color adjustment

--- components/Theme.js
+++ components/Theme.js
@@ -3,9 +3,10 @@ export default {
   data: () => ({
     themes: {
       blue: {
-        "--textarea-bg": "hsl(200, 19%, 18%)",
-        "--textarea-color": "hsla(0, 0%, 100%, 0.8)",
-        "--textarea-color-disabled": "hsla(0, 0%, 100%, 0.3)",
+        "--me-textarea-bg": "hsl(200, 19%, 18%)",
+        "--user-textarea-bg": "hsl(200, 19%, 28%)",
+        "--me-textarea-color": "hsla(0, 0%, 100%, 0.8)",
+        "--user-textarea-color": "hsla(0, 0%, 100%, 0.6)",
         "--aside-bg": "hsl(200, 19%, 12%)",
         "--active": "green",
         "--inactive": "hsla(0, 0%, 100%, 0.2)",
@@ -14,7 +15,7 @@ export default {
         "--me-textarea-bg": "hsl(0, 62%, 79%)",
         "--user-textarea-bg": "hsl(0, 62%, 89%)",
         "--me-textarea-color": "hsla(0, 0%, 0%, 0.8)",
-        "--user-textarea-color": "hsla(0, 0%, 0%, 0.5)",
+        "--user-textarea-color": "hsla(0, 0%, 0%, 0.6)",
         "--aside-bg": "hsl(0, 62%, 95%)",
         "--active": "#56e39f",
         "--inactive": "hsla(0, 0%, 0%, 0.2)",
