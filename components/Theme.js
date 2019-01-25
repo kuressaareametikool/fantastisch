f24c010cf7ee9c4d401c72394d402e91938f5aeb
aivivosa <46887746+aivivosa@users.noreply.github.com>
2019-01-25 08:44:06 +0200

    changed disable color

--- components/Theme.js
+++ components/Theme.js
@@ -5,7 +5,7 @@ export default {
       blue: {
         "--textarea-bg": "hsl(200, 19%, 18%)",
         "--textarea-color": "hsla(0, 0%, 100%, 0.8)",
-        "--textarea-color-disabled": "hsla(0, 0%, 100%, 0.2)",
+        "--textarea-color-disabled": "hsla(0, 0%, 100%, 0.3)",
         "--aside-bg": "hsl(200, 19%, 12%)",
         "--active": "green",
         "--inactive": "hsla(0, 0%, 100%, 0.2)",
@@ -13,7 +13,7 @@ export default {
       pink: {
         "--textarea-bg": "hsl(0, 62%, 89%)",
         "--textarea-color": "hsla(0, 0%, 0%, 0.8)",
-        "--textarea-color-disabled": "hsla(0, 0%, 0%, 0.2)",
+        "--textarea-color-disabled": "hsla(0, 0%, 0%, 0.5)",
         "--aside-bg": "hsla(0, 62%, 89%, 0.36)",
         "--active": "#56e39f",
         "--inactive": "hsla(0, 0%, 0%, 0.2)",
