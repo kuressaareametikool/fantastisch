fd9b3b3eb99f84431b5b745213c15f4f1d781a3e
kristjanjansen <kristjan.jansen@gmail.com>
2019-01-28 11:17:32 +0200

    reverting back the temporary fix

--- utils.js
+++ utils.js
@@ -1,25 +1,4 @@
-/*
-
-import { random } from "./helpers.js";
-
-  Generate a random number between "from" and "to" values
-
-  Usage:
-
-      random(0,100)
-
-  But default it outputs integer values. To get floating point value use:
-
-      random(0,100, true)
-
-*/
-
-export function random(from, to, float = false) {
-  const r = from + Math.random() * (to - from);
-  return float ? r : Math.floor(r, 2);
-}
-
-export function print(value) {
+export function print(value = '<br>') {
   document.getElementsByTagName(
     "body"
   )[0].innerHTML += value
